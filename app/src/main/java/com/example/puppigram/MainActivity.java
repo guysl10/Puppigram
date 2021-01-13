8f6554bf772ea2302ec12136f1860d116091d036
guysl10 <guy.sl10@gmail.com>
2021-01-13 20:01:07 +0200

    Create empty FeedFragment & PostFragment.

--- app/src/main/java/com/example/puppigram/MainActivity.java
+++ app/src/main/java/com/example/puppigram/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.puppigram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FeedFragment feed_fragment = new FeedFragment();
+        FragmentManager feed_manager = getSupportFragmentManager();
+        FragmentTransaction feed_tran = feed_manager.beginTransaction();
+        feed_tran.add(R.id.main_container, feed_fragment);
+        feed_tran.commit();
     }
 }
---
+++ app/src/main/java/com/example/puppigram/PostFragment.java
@@ -0,0 +1,20 @@
+package com.example.puppigram;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//Responsible to handle all simple post issues
+public class PostFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+}
