81bbe7d0ca5d6bd003a7ebc7c048e116aa138d24
guysl10 <guy.sl10@gmail.com>
2021-02-27 14:25:34 +0200

    Mostly edit comments for all models. Add comment class

--- app/src/main/java/com/example/puppigram/model/AdminUser.java
+++ app/src/main/java/com/example/puppigram/model/AdminUser.java
@@ -1,8 +1,13 @@
 package com.example.puppigram.model;
 
+
+/**
+ * Represent a Admin kind of user.
+ * This user have full access to the application and can manage the application.
+ */
 public class AdminUser extends AbstractUser{
     public AdminUser(int id, String name, String gender, String mail, String hash_password) {
-        super(id, name, gender, mail, hash_password);
+        super(id, name, gender, mail, hash_password, null);
     }
 
     @Override
--- app/src/main/java/com/example/puppigram/model/TextPost.java
+++ app/src/main/java/com/example/puppigram/model/TextPost.java
@@ -1,6 +1,10 @@
 package com.example.puppigram.model;
 
+/**
+ * Represent a text kind of post.
+ */
 public class TextPost extends AbstractPost{
+
     public TextPost(int id, int owner_id, String title) {
         super(id, owner_id, title);
     }
