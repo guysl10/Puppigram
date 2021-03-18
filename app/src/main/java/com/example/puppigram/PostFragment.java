af64eaaa7933313622e16726baa59ba0c1ddf4be
Guy Salomon <guy.sl10@gmail.com>
2021-03-18 22:48:16 +0200

    create feed ui

--- app/src/main/java/com/example/puppigram/PostFragment.java
+++ app/src/main/java/com/example/puppigram/PostFragment.java
@@ -2,6 +2,7 @@ package com.example.puppigram;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,8 +11,14 @@ import android.view.ViewGroup;
 
 //Responsible to handle all simple post issues
 public class PostFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState){
+//
+        }
+    }
 
-//    @Override
+    //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 //                             Bundle savedInstanceState) {
 //        // Inflate the layout for this fragment
