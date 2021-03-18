d4d3328c8e8a9cd9cdc673578e76b26535b306d0
Guy Salomon <guy.sl10@gmail.com>
2021-03-18 23:53:17 +0200

    more functionallities for post (ui & model)

--- app/src/main/java/com/example/puppigram/PostFragment.java
+++ app/src/main/java/com/example/puppigram/PostFragment.java
@@ -1,28 +1,71 @@
 package com.example.puppigram;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 //Responsible to handle all simple post issues
 public class PostFragment extends Fragment {
+    private TextView post_description, post_username, post_count_likers;
+    private ImageView post_img, post_user_img, post_liker_img;
+    private ProgressBar post_progressBar;
+    private MainActivity mainActivity;
+    private View post;
+    private int post_id;
+
+    public PostFragment(){};
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState){
-//
-        }
+        super.onCreate(savedInstanceState);
+        this.mainActivity = (MainActivity) getActivity();
     }
 
-    //    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-////        return inflater.inflate(R.layout.fragment_post, container, false);
-//
-//    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(
+                R.layout.feed_post_row,
+                container,
+                false
+        );
+
+        this.post_description = view.findViewById(R.id.post_description);
+        this.post_username = view.findViewById(R.id.post_username);
+        this.post_count_likers = view.findViewById(R.id.post_count_likers);
+
+        this.post_img = view.findViewById(R.id.post_img);
+        this.post_liker_img = view.findViewById(R.id.post_liker_img);
+        this.post_user_img = view.findViewById(R.id.post_user_img);
+        this.post_progressBar = view.findViewById(R.id.post_progressBar);
+
+
+        return view;
+
+    }
+
+    private void updateView() {
+//        populatePostView(
+//                mainActivity,
+//                mainActivity.getApplicationContext(),
+//                post,
+//                post_liker_img,
+//                post_description,
+//                post_user_img,
+//                post_username,
+//                post_img,
+//                post_count_likers,
+//                post_progressBar
+//        );
+    }
 }
---
+++ app/src/main/java/com/example/puppigram/repos/PostRepo.java
@@ -0,0 +1,32 @@
+package com.example.puppigram.repos;
+
+import android.net.Uri;
+
+import com.example.puppigram.model.ImagePost;
+
+import java.util.List;
+
+public interface PostRepo {
+    String createNewPost();
+
+    Task addPost(ImagePost post);
+
+    DatabaseReference getPost(String id);
+
+    void getPosts(GetAllPostsListener getAllPostsListener);
+
+    void updateYummies(String key, List<String> list);
+
+    Task deletePost(String key);
+
+    UploadTask uploadPhoto(String path, Uri photo);
+
+    Task getDownloadPhotoUrl(String path);
+
+    void deleteAllPostsByUserKey(String key);
+
+    interface GetAllPostsListener {
+        void onSuccsess(ImagePost.PostList posts);
+    }
+
+}
