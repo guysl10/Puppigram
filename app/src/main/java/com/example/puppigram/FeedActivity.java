16611d3adb88faf39b98982b2b50dcaa5aca1b4f
Guy Salomon <guy.sl10@gmail.com>
2021-03-19 20:36:07 +0200

    convert listview to be recyclerview for feed.

--- app/src/main/java/com/example/puppigram/FeedActivity.java
+++ app/src/main/java/com/example/puppigram/FeedActivity.java
@@ -2,86 +2,89 @@ package com.example.puppigram;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.puppigram.model.ImagePost;
+
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
 //Responsible to handle all feed issues.
 public class FeedActivity extends AppCompatActivity {
+    RecyclerView posts;
+    List<ImagePost> imagePosts;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_feed);
-//
-////        RecyclerView posts = findViewById(R.id.postslist_recyclerv);
-        ListView posts = findViewById(R.id.feed_posts_lists);
-        PostAdapter postAdapter = new PostAdapter();
-        posts.setAdapter(postAdapter);
 
-//        posts.setHasFixedSize(true);
-//        layoutManager = new LinearLayoutManager(this);
-//        posts.setLayoutManager(layoutManager);
+        posts = findViewById(R.id.feed_posts_recycler_list);
+        posts.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        posts.setLayoutManager(layoutManager);
+
+        imagePosts = new LinkedList<ImagePost>();
+
+        //TODO: to remove after connecting to db and apply getting all posts.
+        for(int i=0;i<100;i++) {
+            ImagePost post = new ImagePost(12,13,"bla", "bla2");
+            imagePosts.add(post);
+        }
+
+        PostRecyclerAdapter adapter = new PostRecyclerAdapter();
+        posts.setAdapter(adapter);
     }
 
+    static class PostViewHolder extends RecyclerView.ViewHolder{
+        TextView username;
+        TextView description;
+        TextView likers;
+        ImageView post_img;
+        ImageView user_img;
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            description = itemView.findViewById(R.id.post_description);
+            likers = itemView.findViewById(R.id.post_count_likers);
+            username = itemView.findViewById(R.id.post_username);
+            user_img = itemView.findViewById(R.id.post_user_img);
+            post_img = itemView.findViewById(R.id.post_img);
+        }
+    }
 
-    class PostAdapter extends BaseAdapter{
-//        List<ImagePost> imagePosts = new LinkedList<ImagePost>();
-        List<String> imagePosts = new LinkedList<String>();
-        PostAdapter(){
-            //TODO: get all posts from db.
-            for(int i=0;i<10;++i){
-                imagePosts.add("element #"+i);//add post
-            }
+    class PostRecyclerAdapter extends RecyclerView.Adapter<PostViewHolder>{
+        @NonNull
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.feed_post_row, null);
+            PostViewHolder holder = new PostViewHolder(view);
+
+            return holder;
         }
 
         @Override
-        public int getCount() {
+        public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+            ImagePost post = imagePosts.get(position);
+            holder.description.setText(post.getDescription());
+            //TODO: add all posts items to set for the recyclerview feed.
+
+        }
+
+        @Override
+        public int getItemCount() {
             return imagePosts.size();
         }
-
-        @Override
-        public Object getItem(int position) {
-            return imagePosts.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int post_id, View view, ViewGroup viewGroup) {
-            View v = getLayoutInflater().inflate(R.layout.feed_post_row, null);
-
-            TextView description = v.findViewById(R.id.post_description);
-            description.setText(imagePosts.get(post_id));//.getDescription());
-
-//            TextView likers = v.findViewById(R.id.post_count_likers);
-//            likers.setText(imagePosts.get(post_id).getNumLikers());
-//
-//            TextView username = v.findViewById(R.id.post_username);
-//            //TODO: chanage for getting the username instead of owner_id.
-//            username.setText(imagePosts.get(post_id).getOwner_id());
-//
-//            ImageView user_img = v.findViewById(R.id.post_user_img);
-//            //TODO: chanage for getting the user_img instead of string, has to be drawable.
-////            user_img.setImageDrawable();
-//
-//            ImageView post_img = v.findViewById(R.id.post_img);
-//            //TODO: chanage for getting the post_img instead of string.
-////            post_img.setImageDrawable(imagePosts.get(post_id).getImage());
-
-
-            return v;
-        }
     }
-
 }
