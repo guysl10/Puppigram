b0ba9993998a82006cd3c182e33b2558ec246267
Guy Salomon <guy.sl10@gmail.com>
2021-04-16 15:17:24 +0300

    add logout. need to apply logout.

---
+++ app/src/main/java/com/example/puppigram/fragments/SettingsFragment.java
@@ -0,0 +1,38 @@
+package com.example.puppigram.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.puppigram.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends Fragment {
+    Button logoutBtn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        logoutBtn = view.findViewById(R.id.logout_btn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: apply logout functionality.
+            }
+        });
+
+        return view;
+
+    }
+}
