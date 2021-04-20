4ef50f7abf31869c5c2b7b21f348a46c3f00b9e3
Guy Salomon <guy.sl10@gmail.com>
2021-04-20 20:27:59 +0300

    finish apply edit post

--- app/src/main/java/com/example/puppigram/fragments/EditPostFragment.java
+++ app/src/main/java/com/example/puppigram/fragments/EditPostFragment.java
@@ -1,8 +1,10 @@
 package com.example.puppigram.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -15,6 +17,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -22,13 +25,14 @@ import com.example.puppigram.R;
 import com.example.puppigram.activities.MainActivity;
 import com.example.puppigram.model.post.ImagePost;
 import com.example.puppigram.model.post.PostsModel;
-import com.example.puppigram.model.user.User;
 import com.example.puppigram.model.user.UsersModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
+import org.w3c.dom.Text;
+
+import java.util.UUID;
 
 import static android.app.Activity.RESULT_OK;
 import static com.example.puppigram.utils.PhotoUtil.REQUEST_IMAGE_CAPTURE;
@@ -50,6 +54,8 @@ public class EditPostFragment extends Fragment {
     FirebaseUser currentUser;
     ProgressBar spinner;
     ImagePost editablePost;
+    ImageView captureBtn;
+    TextView cancelBtn;
 
     public EditPostFragment() {
         // Required empty public constructor
@@ -73,7 +79,8 @@ public class EditPostFragment extends Fragment {
         username = view.findViewById(R.id.edit_username_text);
         removeContentImg = view.findViewById(R.id.edit_remove_img);
         deletePostBtn = view.findViewById(R.id.edit_delete_post_text);
-        ImageView captureBtn = view.findViewById(R.id.edit_capture_img);
+        captureBtn = view.findViewById(R.id.edit_capture_img);
+        cancelBtn = view.findViewById(R.id.edit_cancel_text);
 
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -100,9 +107,14 @@ public class EditPostFragment extends Fragment {
                         checkAndRequestPermissionForCamera(postImg)
         );
 
+        cancelBtn.setOnClickListener(v -> cancel());
+
         removeContentImg.setOnClickListener(v -> removeContent());
         deletePostBtn.setOnClickListener(v-> deletePost());
-        editBtn.setOnClickListener(v -> editPost(view));
+        editBtn.setOnClickListener(v -> {
+            editPost(view);
+            Navigation.findNavController(view).navigate(R.id.action_editPostFragment_to_feedFragment);
+        });
 
         spinner.setVisibility(View.INVISIBLE);
         editBtn.setEnabled(true);
@@ -112,10 +124,20 @@ public class EditPostFragment extends Fragment {
         return view;
     }
 
-
+    private void cancel(){
+        spinner.setVisibility(View.VISIBLE);
+        cancelBtn.setEnabled(false);
+        removeContent();
+        Navigation.findNavController(getView()).navigate(R.id.action_editPostFragment_to_feedFragment);
+        cancelBtn.setEnabled(true);
+        spinner.setVisibility(View.INVISIBLE);
+    }
     private void removeContent() {
         description.setText("");
         postImg.setImageResource(0);
+        userImage.setImageResource(0);
+        username.setText("");
+        description.setText("");
     }
 
     @SuppressLint("ShowToast")
@@ -156,23 +178,54 @@ public class EditPostFragment extends Fragment {
         if (description.getText() == null)
             description.setText("");
 
-//        File tempFile = new File(this.postImg.toString());
-//        Uri photoUri = Uri.fromFile(tempFile);
-//        String userUid = this.currentUser.getUid();
-//        String photoUid = UUID.randomUUID().toString();
+        if (postImg.getDrawable() == null) {
+            Log.d("TAG", "upload_post: No image selected");
+            Toast.makeText(view.getContext(), "No image selected", Toast.LENGTH_LONG).show();
+            captureBtn.setEnabled(true);
+            return;
+        }
+        if (description.getText() == null)
+            description.setText("");
 
-//        ImagePost new_post = new ImagePost(photoUid, userUid, description.getText().toString(), photoUri);
-//        PostsModel.instance.updatePost(new_post, () -> {
-//            Log.d("TAG", "editPost: Post was uploaded");
-//            Toast.makeText(view.getContext(), "Post was updated successfully", Toast.LENGTH_LONG).show();
-//            spinner.setVisibility(View.INVISIBLE);
-//            uploadBtn.setEnabled(true);
-//            description.setEnabled(true);
-//            deletePostBtn.setEnabled(true);
-//            uploadBtn.setEnabled(true);
-//            description.setEnabled(true);
-//            deletePostBtn.setEnabled(true);
-//            removeContentImg.setEnabled(true);
-//        });
+        String userUid = this.currentUser.getUid();
+        String photoUid = UUID.randomUUID().toString();
+
+        Bitmap postBitmap = ((BitmapDrawable)this.postImg.getDrawable()).getBitmap();
+
+        PostsModel.instance.uploadImage(postBitmap, photoUid, url -> {
+            if (url == null){
+                displayFailedError();
+            }else{
+                ImagePost new_post = new ImagePost(
+                        photoUid,
+                        userUid,
+                        description.getText().toString(),
+                        url,
+                        System.currentTimeMillis()
+                );
+                PostsModel.instance.updatePost(new_post, () -> {
+                    Log.d("TAG", "upload_post: Post was updated");
+                    Toast.makeText(view.getContext(), "Post was updated", Toast.LENGTH_LONG).show();
+                    removeContent();
+                    captureBtn.setEnabled(true);
+                    removeContentImg.setEnabled(true);
+                    description.setEnabled(true);
+                    spinner.setVisibility(View.INVISIBLE);
+                });
+            }
+        });
+    }
+
+    private void displayFailedError() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Operation Failed");
+        builder.setMessage("Saving image failed, please try again later...");
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
     }
 }
